24aeddc3dc6e3a5bcafb4dc606dd7900945e3dee
alexandrajimenezc <artesalexj@gmail.com>
2022-04-21 18:24:54 +0200

    [ADD] first task

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿//Escribe un programa que reciba tu nombre y lo escriba por consola.
+Console.WriteLine("Introduce tu nombre: ");
+string name = Console.ReadLine();
+Console.WriteLine(name);
+
+//Escribe un programa que tome la hora y la escriba por pantalla.
+string getTime = DateTime.Now.ToString("hh:mm:ss tt");
+Console.WriteLine("La hora es: " + getTime);
